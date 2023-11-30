094da0af89b0be4513b0a906eddef550be7481a1
Del <deldelnd02@gmail.com>
2023-11-30 11:55:56 +0100

    Created DataLog

---
+++ Easysave/Models/DataLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Easysave.Models
+{
+	public class DataLog
+	{
+        public string saveName;
+        public string sourceDir;
+        public string targetDir;
+        public string saveSize;
+        public long saveDuration;
+
+        public DataLog()
+		{
+		}
+	}
+}
+
