d94ec76597408f2f2e18f1b0f022f99604347c51
Del <deldelnd02@gmail.com>
2023-11-30 10:31:42 +0100

    Debugging State class methods (path problem)

--- Easysave/Models/State.cs
+++ Easysave/Models/State.cs
@@ -1,35 +1,36 @@
 ﻿using System;
-using System.Xml;
 using System.Text.Json;
 using EasySave.ViewModels;
-
-using EasySaveApp.Models;
 
 namespace EasySave.Models
 {
     public class State
     {
         private string ProjectDir = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+
         public DataState DataState { get; set; }
+
         private Config configObj = Config.getConfig();
 
-        public State()
-        {
+        public State() { }
 
-        }
+
 
         public void CreateState(int id)
         {
-            string jsonString = File.ReadAllText(ProjectDir + configObj.SaveStateDir);
+            string stateFilePath = ProjectDir + configObj.SaveStateDir;
+            Console.WriteLine(stateFilePath);
+            Console.WriteLine(configObj);
+            Console.WriteLine(configObj.SaveStateDir);
+            string jsonString = File.ReadAllText(stateFilePath);
 
+            DataState[] dataStateArr = JsonSerializer.Deserialize<DataState[]>(jsonString);
             List<DataState> stateList = new List<DataState>();
-            DataState[] dataStateList = JsonSerializer.Deserialize<DataState[]>(jsonString);
+            DataState dataStateEntry = new DataState(id);
 
-            DataState test = new DataState(id);
-            stateList.Add(test);
 
-            stateList.AddRange(dataStateList);
-
+            stateList.Add(dataStateEntry);
+            stateList.AddRange(dataStateArr);
 
             string serializedJSON = JsonSerializer.Serialize(stateList) + Environment.NewLine;
             Console.WriteLine(serializedJSON);
@@ -39,7 +40,7 @@ namespace EasySave.Models
 
 
 
-        public void UpdateStatefile (int saveId, string saveName, string time, string type, bool saveState, string sourcePath, string targetPath, int fileNumber, long filesSize, float progress, int remainingFiles, long remainingFilesSize)
+        public void UpdateStatefile(int saveId, string saveName, string time, string type, bool saveState, string sourcePath, string targetPath, int fileNumber, long filesSize, float progress, int remainingFiles, long remainingFilesSize)
         {
             List<DataState> stateList = new List<DataState>();
             if (!File.Exists(ProjectDir + configObj.SaveStateDir))
@@ -51,7 +52,6 @@ namespace EasySave.Models
             Console.WriteLine(jsonString);
             if (jsonString.Length != 0)
             {
-
                 DataState[] dataStateList = JsonSerializer.Deserialize<DataState[]>(jsonString);
 
                 foreach (var saveSlot in dataStateList)
