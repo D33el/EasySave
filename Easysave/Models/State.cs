ac89fa67da4b3ce887287dc35ca99115895cdaf7
Del <deldelnd02@gmail.com>
2023-12-02 19:17:22 +0100

    fixed createState method, simplified DeleteSave logic

--- Easysave/Models/State.cs
+++ Easysave/Models/State.cs
@@ -18,16 +18,23 @@ namespace Easysave.Models
             string stateFilePath = ProjectDir + configObj.SaveStateDir;
             string jsonString = File.ReadAllText(stateFilePath);
 
+            if (!File.Exists(ProjectDir + configObj.SaveStateDir))
+            {
+                File.Create(configObj.SaveStateDir).Close();
+                using StreamWriter sw = File.CreateText(configObj.SaveStateDir);
+                sw.Write("[]");
+            }
+
             DataState[] dataStateArr = JsonSerializer.Deserialize<DataState[]>(jsonString);
             List<DataState> stateList = new List<DataState>();
+
             //Adding the old state entries to the list
             stateList.AddRange(dataStateArr);
             //Adding the new state entry to the list
             stateList.Add(DataState);
 
-
             string serializedJSON = JsonSerializer.Serialize(stateList) + Environment.NewLine;
-            Console.WriteLine(serializedJSON);
+            
 
             File.WriteAllText(ProjectDir + configObj.SaveStateDir, serializedJSON);
         }
@@ -80,8 +87,6 @@ namespace Easysave.Models
 
             string jsonString = File.ReadAllText(ProjectDir + configObj.SaveStateDir);
             List<DataState> stateList = JsonSerializer.Deserialize<List<DataState>>(jsonString);
-
-            Console.WriteLine(DataState.SaveId);
 
             stateList.RemoveAll(save => save.SaveId == DataState.SaveId);
 
