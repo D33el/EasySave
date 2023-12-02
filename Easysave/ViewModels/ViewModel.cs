6c1fe76a33bb25e23ce35871c5df230fa2c98679
amiiinee77 <runcodedz@gmail.com>
2023-12-02 17:23:23 +0100

    Fix Id attributions + checkSavesNumber + Colors

--- Easysave/ViewModels/ViewModel.cs
+++ Easysave/ViewModels/ViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.IO;
 using System.Text.Json;
+using System.Diagnostics;
 using Easysave.Models;
 using Easysave.Views;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection;
 
 namespace Easysave.ViewModels
 {
@@ -22,17 +25,19 @@ namespace Easysave.ViewModels
             if (inputObj.Type == "full")
             {
                 DataState[] saveArr = GetSavelist();
-                int savesNb = saveArr.Length;
-                if (savesNb < 5)
+                int MaxId = 0;
+                
+                foreach (var item in saveArr)
                 {
-                    int id = savesNb + 1;
-                    save.SaveId = id;
+                    if (item.SaveId > MaxId)
+                    {
+                        MaxId = item.SaveId;
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("TODO : erreur il existe 5 sauvegarde");
-                    return;
-                }
+
+                int id = MaxId + 1;
+                save.SaveId = id;
+
             }
             else
             {
@@ -78,26 +83,40 @@ namespace Easysave.ViewModels
             return result;
         }
 
+        public int checkSavesNumber()
+        {
+            ViewModel viewmodel = new ViewModel();
+            DataState[] saveArr = viewmodel.GetSavelist();
+            int savesNb = saveArr.Length;
+            return savesNb;
+        }
+
         public void NavigateTo(int page)
         {
 
             View view = new View();
             Navigation navigation = new Navigation();
 
+            string lang = configObj.Language;
+
+            int saveNumber = checkSavesNumber();
 
             switch (page)
             {
                 case 1: // Show the saves
-                    navigation.ShowSaveList();
+                    if (saveNumber == 0) { Console.ForegroundColor = ConsoleColor.Red;  if (lang == "en") { Console.Clear(); Console.WriteLine(" No backup available !"); } else { Console.Clear();  Console.WriteLine(" Aucune sauvegarde disponible !"); } } else {  navigation.ShowSaveList(); }
+                    Console.ForegroundColor = ConsoleColor.White;
                     navigation.ShowMainMenu();
                     break;
                 case 2: // Create a new save
-                    navigation.ShowSaveMenu();
+                    if(saveNumber < 5) { navigation.ShowSaveMenu(); } else { Console.ForegroundColor = ConsoleColor.Red; if (lang == "en") { Console.Clear(); Console.WriteLine(" Error. There are 5 saves!"); } else { Console.Clear(); Console.WriteLine(" Erreur. Il existe 5 sauvegardes !"); }}
+                    Console.ForegroundColor = ConsoleColor.White;
                     navigation.ShowMainMenu();
                     break;
 
                 case 3: // Delete a save
-                    navigation.ShowDeleteMenu();
+                    if(saveNumber == 0) { if (lang == "en") { Console.Clear(); Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine(" No backup available !"); } else { Console.Clear(); Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine(" Aucune sauvegarde disponible !"); } } else { navigation.ShowDeleteMenu(); }
+                    Console.ForegroundColor = ConsoleColor.White;
                     navigation.ShowMainMenu();
                     break;
 
@@ -112,6 +131,7 @@ namespace Easysave.ViewModels
                         DataConfig data = view.GetParametersInput(false, step);
                         configObj.DataConfig = data;
                         configObj.SaveConfig();
+                        configObj.LoadConfig();
                         NavigateTo(4);
                     }
 
