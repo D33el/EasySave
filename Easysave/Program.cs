9893f90353ce857d875b6df0279b2b76fbd9bf83
Del <deldelnd02@gmail.com>
2023-12-12 19:04:33 +0100

    replaced formatfilesize

--- Easysave/Program.cs
+++ Easysave/Program.cs
@@ -21,5 +21,21 @@ namespace EasySave
             }
             _view.ShowMainMenu();
         }
+
+        public static string FormatFileSize(long sizeInBytes)
+        {
+            string[] sizeSuffixes = { "B", "KB", "MB", "GB" };
+
+            int i = 0;
+            double size = sizeInBytes;
+
+            while (size >= 1024 && i < sizeSuffixes.Length - 1)
+            {
+                size /= 1024;
+                i++;
+            }
+
+            return $"{size:N2} {sizeSuffixes[i]}";
+        }
     }
 }
