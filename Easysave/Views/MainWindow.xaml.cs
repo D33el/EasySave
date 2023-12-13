a697b2b1f33834576fed419f0756b68b4e897a98
amiiinee77 <runcodedz@gmail.com>
2023-12-13 10:27:08 +0100

    Fist seetings page + Getting path from Explorer

--- Easysave/Views/MainWindow.xaml.cs
+++ Easysave/Views/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Ookii.Dialogs.Wpf;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,52 +16,88 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections;
 using EasySave.ViewModels;
+using EasySave;
 
 namespace wpftest
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
     public partial class MainWindow : Window
     {
+       
+        private delegate void  writeOnTextBlock(string textboxName, string value);
 
+
+
+
+        Config _config = Config.GetConfig();
         SaveViewModel viewModel = new SaveViewModel();
+
+
         public MainWindow()
         {
             InitializeComponent();
-            WelcomePage.Visibility = Visibility.Collapsed;
-            HomePage.Visibility = Visibility.Visible;
-            SettingsPage.Visibility = Visibility.Collapsed;
-            SavePage.Visibility = Visibility.Collapsed;
+            bool configExists = _config.CheckConfig();
+            if (!configExists)
+            {
+                ShowWelcomePage();
+            }
+            else
+            {
+                ShowHomePage();
+            }
+
             displaySaveList();
-            
+            displayParameters();
         }
+
+
+
+
+
+
 
         public void displaySaveList()
         {
             SaveList.ItemsSource = viewModel.GetSaveList();
-            
         }
 
+        public void displayParameters()
+        {
+
+        }
+
+        private void Commencer_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem selectedLanguage= LangueSetting.SelectedItem as ComboBoxItem;
+            ComboBoxItem selectedType = LogsType.SelectedItem as ComboBoxItem;
+
+            string Langue = selectedLanguage.Tag.ToString();
+            string targetsave = TargetSave.Text;
+            string targetlogs = TargetLogs.Text;
+            string logstype = selectedType.Tag.ToString();
+            _config.Language = Langue;
+            _config.TargetDir = targetsave;
+            _config.LogsDir = targetlogs;
+            _config.LogsType = logstype;
+            _config.SaveConfig();
+            ShowHomePage() ;
+
+        }
         private void Home_Click(object sender, RoutedEventArgs e)
         {
-            HomePage.Visibility = Visibility.Visible;
-
-            SettingsPage.Visibility = Visibility.Collapsed;
-            SavePage.Visibility = Visibility.Collapsed;
+            ShowHomePage();
         }
         private void Settings_Click(object sender, RoutedEventArgs e)
         {
-            SettingsPage.Visibility = Visibility.Visible;
-
-            HomePage.Visibility = Visibility.Collapsed;
-            SavePage.Visibility = Visibility.Collapsed;
+            ShowSettingsPage();
         }
         private void Save_Click(object sender, RoutedEventArgs e)
         {
-            SavePage.Visibility = Visibility.Visible;
-            HomePage.Visibility = Visibility.Collapsed;
-            SettingsPage.Visibility = Visibility.Collapsed;
+            ShowSavePage();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -71,7 +108,33 @@ namespace wpftest
         private void OpenExplorer_Click(object sender, RoutedEventArgs e)
         {
 
+                var dialog = new VistaFolderBrowserDialog();
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            dialog.SelectedPath = desktopPath;
+
+            if (dialog.ShowDialog() == true)
+            {
+
+                string selectedFolderPath = dialog.SelectedPath;
+
+                Button button = sender as Button;
+
+                if (button != null)
+                {
+                    string buttonTag = button.Tag.ToString();
+
+                    TextBox associatedTextBox = this.FindName(buttonTag) as TextBox;
+
+                    if (associatedTextBox != null)
+                    {
+                        associatedTextBox.Text = selectedFolderPath;
+                    };
+                    
+                }
+
+            }
         }
+
 
         private void CreateSave_Click(object sender, RoutedEventArgs e)
         {
@@ -79,7 +142,7 @@ namespace wpftest
             string savename = SaveName.Text;
             string savetype = selectedComboBoxItem.Tag.ToString();
             string sourcepath = SourcePath.Text;
-            viewModel.InitializeSave(savename,savetype,sourcepath,0);
+            viewModel.InitializeSave(savename, savetype, sourcepath, 0);
             SaveName.Clear();
             SaveType.SelectedItem = null;
             SourcePath.Clear();
@@ -132,5 +195,39 @@ namespace wpftest
 
             return null;
         }
+
+
+
+        public void ShowHomePage()
+        {
+            WelcomePage.Visibility = Visibility.Collapsed;
+            HomePage.Visibility = Visibility.Visible;
+            SettingsPage.Visibility = Visibility.Collapsed;
+            SavePage.Visibility = Visibility.Collapsed;
+        }
+        public void ShowSettingsPage()
+        {
+            WelcomePage.Visibility = Visibility.Collapsed;
+            HomePage.Visibility = Visibility.Collapsed;
+            SettingsPage.Visibility = Visibility.Visible;
+            SavePage.Visibility = Visibility.Collapsed;
+        }
+
+        public void ShowSavePage()
+        {
+            WelcomePage.Visibility = Visibility.Collapsed;
+            HomePage.Visibility = Visibility.Collapsed;
+            SettingsPage.Visibility = Visibility.Collapsed;
+            SavePage.Visibility = Visibility.Visible;
+        }
+
+        public void ShowWelcomePage()
+        {
+            WelcomePage.Visibility = Visibility.Visible;
+            HomePage.Visibility = Visibility.Collapsed;
+            SettingsPage.Visibility = Visibility.Collapsed;
+            SavePage.Visibility = Visibility.Collapsed;
+        }
+
     }
 }
