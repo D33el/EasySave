7d0effb3262015a0df748f67ffb7d168736b69e6
Del <deldelnd02@gmail.com>
2023-12-19 12:55:16 +0100

    Added Mutex for Mono-instance

--- Easysave/App.xaml.cs
+++ Easysave/App.xaml.cs
@@ -13,6 +13,29 @@ namespace EasySave
     /// </summary>
     public partial class App : Application
     {
+        private Mutex _appMutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool createdNew;
+            _appMutex = new Mutex(true, "Easysave_V3", out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("Another instance of the application is already running.");
+                Current.Shutdown();
+                return;
+            }
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _appMutex?.ReleaseMutex();
+            _appMutex?.Dispose();
+            base.OnExit(e);
+        }
     }
 
 
